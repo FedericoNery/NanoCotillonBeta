ce2f4018a5aa63a95c00d984637f7de7b294a4b4
FedericoNery <fedegnery@gmail.com>
2017-07-08 22:23:33 -0300

    Creacion de Funciones de ingreso, sus respectivas validaciones, etc.

--- ingreso_de_datos.py
+++ ingreso_de_datos.py
@@ -2,24 +2,116 @@ import string
 
 def ingresoCodigoDeBarras():
     # Leo entrada del sistema
-    try:
-        print("Esperando lectura de scanner...")
-        codigoDeBarras = input()
-        print("Lectura de scanner recibida, OK!")
-    except:
-        return None
-
-    # Verifico el codigo
-    if len( codigoDeBarras ) == 13:
+    validacionDeIngreso = False
+    while(not validacionDeIngreso):
         try:
-            for digito in codigoDeBarras:
-                if digito not in string.digits:
-                    return None
-            codigoDeBarras = int(codigoDeBarras)
+            codigoDeBarras = input("Esperando lectura de scanner...")
+            print("Lectura de scanner recibida, OK!")
         except:
-            return None
-    else: return None
+            print("ERROR!!")
+            validacionDeIngreso = False
+
+        # Verifico el codigo
+        if (len(codigoDeBarras) == 13):
+            try:
+                for digito in codigoDeBarras:
+                    if digito not in string.digits:
+                        validacionDeIngreso = False
+                codigoDeBarras = int(codigoDeBarras)
+                validacionDeIngreso = True
+            except:
+                print("ERROR!!")
+                validacionDeIngreso = False
+        else:
+            print("ERROR!!")
+            validacionDeIngreso = False
 
     return codigoDeBarras
 
-def
+def ingresoDePrecio():
+    # Leo entrada del sistema
+    validacionDeIngreso = False
+    poseePunto = False
+    while (not validacionDeIngreso):
+        try:
+            precio = float(input("Esperando ingreso del precio..."))
+            print("Ingreso de precio, OK!")
+            validacionDeIngreso = True
+        except:
+            print("ERROR!!")
+            validacionDeIngreso = False
+    return precio
+
+
+def ingresoNumeroDelCliente():
+    # Leo entrada del sistema
+    validacionDeIngreso = False
+    while (not validacionDeIngreso):
+        try:
+            numeroDelCliente = input("Esperando ingreso del numero del cliente...")
+        except:
+            print("ERROR!!")
+            validacionDeIngreso = False
+
+        # Verifico el numero
+        if(len(numeroDelCliente)== 8 or len(numeroDelCliente)== 10 ):
+            try:
+                for digito in str(numeroDelCliente):
+                    if digito not in string.digits:
+                        validacionDeIngreso = False
+            except:
+                print("ERROR!!")
+                validacionDeIngreso = False
+        else:
+            print("ERROR!!")
+            validacionDeIngreso = False
+
+    return numeroDelCliente
+
+def ingresoNombre(nombreDelCampo):
+    # Leo entrada del sistema
+    #  Verifico el nombre del campo afectado
+    validacionDeIngreso = False
+    while (not validacionDeIngreso):
+        if(type(nombreDelCampo) is str):
+            try:
+                nombre = input('Esperando ingreso del nombre de {}...'.format(nombreDelCampo))
+                print('Ingreso de nombre de {}, OK!'.format(nombreDelCampo))
+                validacionDeIngreso = True
+            except:
+                print("ERROR!!")
+                validacionDeIngreso = False
+        else:
+            print("ERROR!!")
+            validacionDeIngreso = False
+
+    return nombre
+
+"""
+def ingresoNombreDelCliente(nombreDelCampo):
+    try:
+        print("Esperando ingreso del nombre del cliente...")
+        nombreDelCliente = input()
+        print("Ingreso de nombre del cliente, OK!")
+    except:
+        return None
+
+def ingresoNombreDeArticulo():
+    try:
+        print("Esperando ingreso del nombre del articulo...")
+        nombreDelArticulo = input()
+        print("Ingreso del nombre del articulo, OK!")
+    except:
+        return None
+
+def ingresoNombreDeLaMarca():
+    try:
+        print("Esperando ingreso del nombre de la marca...")
+        nombreDeLaMarca = input()
+        print("Ingreso del nombre de la marca, OK!")
+    except:
+        return None
+
+"""
+
+
